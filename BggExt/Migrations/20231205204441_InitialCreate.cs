075886adf8d30c1aee1187c560e223a1daf23781
taylort7147 <taylort7147@gmail.com>
2023-12-05 13:02:54 -0700

    Add InitialCreate migration

---
+++ BggExt/Migrations/20231205204441_InitialCreate.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BggExt.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Families",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Families", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImageData = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libraries",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    LastSynchronized = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libraries", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mechanics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mechanics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    YearPublished = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinPlayers = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxPlayers = table.Column<int>(type: "INTEGER", nullable: false),
+                    PlayingTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinPlayTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxPlayTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinAge = table.Column<int>(type: "INTEGER", nullable: false),
+                    AverageWeight = table.Column<double>(type: "REAL", nullable: false),
+                    ThumbnailId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardGames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BoardGames_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BoardGames_Images_ThumbnailId",
+                        column: x => x.ThumbnailId,
+                        principalTable: "Images",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardGameCategory",
+                columns: table => new
+                {
+                    BoardGamesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CategoriesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardGameCategory", x => new { x.BoardGamesId, x.CategoriesId });
+                    table.ForeignKey(
+                        name: "FK_BoardGameCategory_BoardGames_BoardGamesId",
+                        column: x => x.BoardGamesId,
+                        principalTable: "BoardGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BoardGameCategory_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardGameFamily",
+                columns: table => new
+                {
+                    BoardGamesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FamiliesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardGameFamily", x => new { x.BoardGamesId, x.FamiliesId });
+                    table.ForeignKey(
+                        name: "FK_BoardGameFamily_BoardGames_BoardGamesId",
+                        column: x => x.BoardGamesId,
+                        principalTable: "BoardGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BoardGameFamily_Families_FamiliesId",
+                        column: x => x.FamiliesId,
+                        principalTable: "Families",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardGameMechanic",
+                columns: table => new
+                {
+                    BoardGamesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MechanicsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardGameMechanic", x => new { x.BoardGamesId, x.MechanicsId });
+                    table.ForeignKey(
+                        name: "FK_BoardGameMechanic_BoardGames_BoardGamesId",
+                        column: x => x.BoardGamesId,
+                        principalTable: "BoardGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BoardGameMechanic_Mechanics_MechanicsId",
+                        column: x => x.MechanicsId,
+                        principalTable: "Mechanics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibraryData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Location = table.Column<string>(type: "TEXT", nullable: true),
+                    BoardGameId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LibraryUserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibraryData_BoardGames_BoardGameId",
+                        column: x => x.BoardGameId,
+                        principalTable: "BoardGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LibraryData_Libraries_LibraryUserId",
+                        column: x => x.LibraryUserId,
+                        principalTable: "Libraries",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGameCategory_CategoriesId",
+                table: "BoardGameCategory",
+                column: "CategoriesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGameFamily_FamiliesId",
+                table: "BoardGameFamily",
+                column: "FamiliesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGameMechanic_MechanicsId",
+                table: "BoardGameMechanic",
+                column: "MechanicsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGames_ImageId",
+                table: "BoardGames",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGames_ThumbnailId",
+                table: "BoardGames",
+                column: "ThumbnailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryData_BoardGameId",
+                table: "LibraryData",
+                column: "BoardGameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryData_LibraryUserId",
+                table: "LibraryData",
+                column: "LibraryUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoardGameCategory");
+
+            migrationBuilder.DropTable(
+                name: "BoardGameFamily");
+
+            migrationBuilder.DropTable(
+                name: "BoardGameMechanic");
+
+            migrationBuilder.DropTable(
+                name: "LibraryData");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Families");
+
+            migrationBuilder.DropTable(
+                name: "Mechanics");
+
+            migrationBuilder.DropTable(
+                name: "BoardGames");
+
+            migrationBuilder.DropTable(
+                name: "Libraries");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
